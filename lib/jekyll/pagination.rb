39415733594af78a36f8192713e1e9e9707bfec2
Jens Wille <jens.wille@uni-koeln.de>
2011-04-29 17:21:16 +0200

    Switched license to Affero GPL.

--- lib/jekyll/pagination.rb
+++ lib/jekyll/pagination.rb
@@ -3,25 +3,25 @@
 #                                                                             #
 # jekyll-pagination -- Jekyll plugin to extend the pagination generator       #
 #                                                                             #
-# Copyright (C) 2010 University of Cologne,                                   #
-#                    Albertus-Magnus-Platz,                                   #
-#                    50923 Cologne, Germany                                   #
+# Copyright (C) 2010-2011 University of Cologne,                              #
+#                         Albertus-Magnus-Platz,                              #
+#                         50923 Cologne, Germany                              #
 #                                                                             #
 # Authors:                                                                    #
 #     Jens Wille <jens.wille@uni-koeln.de>                                    #
 #                                                                             #
 # jekyll-pagination is free software; you can redistribute it and/or modify   #
-# it under the terms of the GNU General Public License as published by the    #
-# Free Software Foundation; either version 3 of the License, or (at your      #
+# it under the terms of the GNU Affero General Public License as published by #
+# the Free Software Foundation; either version 3 of the License, or (at your  #
 # option) any later version.                                                  #
 #                                                                             #
 # jekyll-pagination is distributed in the hope that it will be useful, but    #
 # WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY  #
-# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for #
-# more details.                                                               #
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public      #
+# License for more details.                                                   #
 #                                                                             #
-# You should have received a copy of the GNU General Public License along     #
-# with jekyll-pagination. If not, see <http://www.gnu.org/licenses/>.         #
+# You should have received a copy of the GNU Affero General Public License    #
+# along with jekyll-pagination. If not, see <http://www.gnu.org/licenses/>.   #
 #                                                                             #
 ###############################################################################
 #++
